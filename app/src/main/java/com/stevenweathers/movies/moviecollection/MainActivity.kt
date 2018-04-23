54eb25525d5d27e863f72084f5f594d49b2919e6
Steven Weathers <steven@weathers.me>
2018-04-23 18:58:58 -0400

    Add Images yay!

--- app/src/main/java/com/stevenweathers/movies/moviecollection/MainActivity.kt
+++ app/src/main/java/com/stevenweathers/movies/moviecollection/MainActivity.kt
@@ -9,6 +9,9 @@ import retrofit2.http.GET
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.Retrofit
 import android.os.StrictMode
+import android.support.v7.widget.DividerItemDecoration
+
+
 
 
 interface MoviesService {
--- app/src/main/java/com/stevenweathers/movies/moviecollection/MovieAdapter.kt
+++ app/src/main/java/com/stevenweathers/movies/moviecollection/MovieAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.movie_list_item.view.*
 
 class MovieAdapter(private val items: List<Movie?>, private val context: Context) : RecyclerView.Adapter<ViewHolder>() {
@@ -15,6 +16,9 @@ class MovieAdapter(private val items: List<Movie?>, private val context: Context
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val movie = items[position]
         holder?.movieTitle?.text = movie?.title
+        Picasso.with(context).load("http://image.tmdb.org/t/p/w154/${movie?.tmdb_image_url}").into(holder?.moviePoster)
+
+
     }
 
     // Gets the number of movies in the list
@@ -25,5 +29,6 @@ class MovieAdapter(private val items: List<Movie?>, private val context: Context
 
 class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
     // Holds the TextView that will add each movie to
-    val movieTitle = view.movie_title
+    val movieTitle = view.movie_title!!
+    val moviePoster = view.movie_poster!!
 }
